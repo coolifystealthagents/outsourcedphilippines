572a0646c122a7530ec4a3587cb187dbaa7eff6a
OutsourcedPhilippines Publishing Agent <publishing@outsourcedphilippines.com>
2026-09-09 03:12:34 +0000

    Publish September 9 blog and research batch

--- app/data.ts
+++ app/data.ts
@@ -6,6 +6,7 @@ import { sep3BlogBasics, sep3BlogPosts } from './blog/sep3-blog-batch';
 import { sep4BlogBasics, sep4BlogPosts } from './blog/sep4-blog-batch';
 import { sep7BlogBasics, sep7BlogPosts } from './blog/sep7-blog-batch';
 import { sep8BlogBasics, sep8BlogPosts } from './blog/sep8-blog-batch';
+import { sep9BlogBasics, sep9BlogPosts } from './blog/sep9-blog-batch';
 
 export const site = {
   domain: 'OutsourcedPhilippines.com',
@@ -123,7 +124,7 @@ const legacyBlogPosts = [
     minutes: 8,
   },] as const;
 
-export const blogPosts = [...legacyBlogPosts, ...allDailyBlogPosts, ...aug31BlogPosts, ...sep1BlogPosts, ...sep2BlogPosts, ...sep3BlogPosts, ...sep4BlogPosts, ...sep7BlogPosts, ...sep8BlogPosts].sort((a, b) => {
+export const blogPosts = [...legacyBlogPosts, ...allDailyBlogPosts, ...aug31BlogPosts, ...sep1BlogPosts, ...sep2BlogPosts, ...sep3BlogPosts, ...sep4BlogPosts, ...sep7BlogPosts, ...sep8BlogPosts, ...sep9BlogPosts].sort((a, b) => {
   const aDate = 'publishedAt' in a && a.publishedAt ? a.publishedAt : '';
   const bDate = 'publishedAt' in b && b.publishedAt ? b.publishedAt : '';
   return bDate.localeCompare(aDate) || a.slug.localeCompare(b.slug);
@@ -309,7 +310,7 @@ export const blogDetails = {
   },
 } as const;
 
-export const blogBasics = { ...legacyBlogBasics, ...allDailyBlogBasics, ...aug31BlogBasics, ...sep1BlogBasics, ...sep2BlogBasics, ...sep3BlogBasics, ...sep4BlogBasics, ...sep7BlogBasics, ...sep8BlogBasics };
+export const blogBasics = { ...legacyBlogBasics, ...allDailyBlogBasics, ...aug31BlogBasics, ...sep1BlogBasics, ...sep2BlogBasics, ...sep3BlogBasics, ...sep4BlogBasics, ...sep7BlogBasics, ...sep8BlogBasics, ...sep9BlogBasics };
 
 export const stats = [
   { label: 'Talent source', value: 'Philippines', note: 'the staffing model is limited to Filipino talent based in the Philippines' },
